3a66187f602bdd09da8852cc91e2ed4df1fbdfa3
duvanflor <duvan.flor@netto.com.co>
2024-12-05 21:43:59 -0500

    Fix: Creation microservices

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
@@ -8,12 +9,22 @@ import { envs } from './config';
 async function bootstrap() {
   const logger = new Logger('Main pokemon service');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: envs.host,
+        port: envs.port,
+      },
+    },
+  );
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-  await app.listen(3000);
+  await app.listen();
+
   logger.log(`Pokemon service running on port ${envs.port}`);
 }
 
